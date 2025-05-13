c70678bac3a0060710dd75c15fcd0ffd5cc4c0d7
LABDM135$ <LABDM135$@laboratorio.ifms.br>
2025-05-13 20:06:52 -0300

    modificação leve

--- view/adicionarCategoria.php
+++ view/adicionarCategoria.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Adicionar Categoria - Social Network</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <style>
+    <style> 
       body {
         background-color: #f8f9fa;
       }
